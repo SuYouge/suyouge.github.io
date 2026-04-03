682ca63c9c0100e3382538b899af21ba5190abb6
suyouge <yogurtsuee@hotmail.com>
2026-04-03 19:13:59 +0800

    init

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Low Tech Land';
+export const SITE_DESCRIPTION = 'Ataraxia is a minimal, accessible Astro v6 theme for building fast, content-focused websites.';
+export const SITE_AUTHOR = 'Su Youge';
